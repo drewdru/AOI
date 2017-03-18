3d03ddd24111b501fbba912765f9f9806b9f3aad
DrewDru <drew.drux@gmail.com>
2017-03-18 16:46:03 +0700

    adaptive filter

---
+++ imageFilters/adaptiveFilter.py
@@ -0,0 +1,87 @@
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+import apertureService
+
+
+def PIL2array(img):
+    return np.array(img.getdata(),
+                    np.uint8).reshape(img.size[1], img.size[0], 3)
+
+def array2PIL(arr, size):
+    mode = 'RGBA'
+    arr = arr.reshape(arr.shape[0]*arr.shape[1], arr.shape[2])
+    if len(arr[0]) == 3:
+        arr = np.c_[arr, 255*np.ones((len(arr),1), np.uint8)]
+    return Image.frombuffer(mode, size, arr.tostring(), 'raw', mode, 0, 1)
+
+def smothAdpmedf(image_array, window, ylength, xlength, vlength, threshold):
+    pixel_count = 0
+    ## loop over image with specified window W
+    for y in range(window, ylength-(window+1)):
+        for x in range(window, xlength-(window+1)):
+        ## populate window, sort, find median
+            filter_window = image_array[y-window:y+window+1,x-window:x+window+1]
+            target_vector = np.reshape(filter_window, ((vlength),))
+            ## internal sort
+            median = demo(target_vector, vlength)
+            ##median = medians_1D.quick_select(target_vector, vlength)
+        ## check for threshold
+            if not threshold > 0:
+                image_array[y,x] = median
+                pixel_count += 1
+            else:
+                scale = np.zeros(vlength)
+                for n in range(vlength):
+                    scale[n] = abs(int(target_vector[n]) - int(median))
+                scale = np.sort(scale)
+                Sk = 1.4826 * (scale[int(vlength/2)])
+                if abs(int(image_array[y,x]) - int(median)) > (threshold * Sk):
+                    image_array[y,x] = median
+                    pixel_count += 1
+
+def adpmedf(image, size, window, threshold):
+    
+    ## set filter window and image dimensions
+    W = 2*window + 1
+    xlength, ylength = size
+    vlength = W*W
+
+    print(np.array(image, dtype=np.uint8), (ylength,xlength))
+    ## create 2-D image array and initialize window
+    filter_window = np.array(np.zeros((W,W)))
+    target_vector = np.array(np.zeros(vlength))
+
+    red, green, blue = img.split()
+    image_arrayR = np.reshape(np.array(red, dtype=np.uint8), (ylength,xlength))
+    smothAdpmedf(image_arrayR, window, ylength, xlength, vlength, threshold)
+
+    image_arrayG = np.reshape(np.array(green, dtype=np.uint8), (ylength,xlength))
+    smothAdpmedf(image_arrayG, window, ylength, xlength, vlength, threshold)
+
+    image_arrayB = np.reshape(np.array(blue, dtype=np.uint8), (ylength,xlength))
+    smothAdpmedf(image_arrayB, window, ylength, xlength, vlength, threshold)
+    
+    return Image.merge("RGB", (Image.fromarray(image_arrayB),
+        Image.fromarray(image_arrayG), Image.fromarray(image_arrayR)))
+
+def demo(target_array, array_length):
+    sorted_array = np.sort(target_array)
+    # print(target_array, array_length)
+    median = sorted_array[int(array_length/2)]
+    return median
+
+img = Image.open('./test3.jpg')
+img = img.convert(mode='RGB')
+img.show()
+
+img = adpmedf(img, img.size, 3, 0)
+
+# Convert array to Image
+# img = Image.fromarray(arr)
+img.show()
